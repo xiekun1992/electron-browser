fa02b295f53932574e3d08862819aef7c28da7cc
xiekun1992 <xkfront@outlook.com>
2020-09-23 00:17:55 +0800

    finish topbar UI

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 
 const subWindows = []
 let currentWindow = null
-const topbarHeight = 115
+const topbarHeight = 86
 const windowBorder = 2
+let parentWindow
 
 function createWindow () {   
   // 创建浏览器窗口
@@ -49,6 +50,7 @@ function createWindow () {
       )
     })
   }
+  return win
 }
 function createSubWindow(parentWindow) {
   const subwin = new BrowserWindow({
@@ -72,7 +74,9 @@ function createSubWindow(parentWindow) {
 }
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
 // 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  parentWindow = createWindow()
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -87,7 +91,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    parentWindow = createWindow()
   }
 })
 
@@ -115,4 +119,17 @@ ipcMain.on('window-reload', (event, arg) => {
 })
 ipcMain.on('window-stop-loading', (event, arg) => {
   currentWindow.webContents.stop()
+})
+ipcMain.on('app-minimize', (event, arg) => {
+  parentWindow.minimize()
+})
+ipcMain.on('app-maximize', (event, arg) => {
+  if (parentWindow.isMaximized()) {
+    parentWindow.unmaximize()
+  } else {
+    parentWindow.maximize()
+  }
+})
+ipcMain.on('app-close', (event, arg) => {
+  parentWindow.close()
 })
